446489181af92f2ae9ad8e2a9e0cd15eab6e5dbf
Mahesh Mestri <mahesh@panicle.tech>
2022-05-03 21:25:06 +0100

    UseState Assertion

---
+++ Tutorials/10-usestate-type-assertion/README.md
@@ -0,0 +1,4 @@
+# UseState Type Assertion
+- here we goona say the type of useState var is like authUser
+- there we are lying but its work
+- we are saying this is empty object as AuthUser
---
+++ Tutorials/10-usestate-type-assertion/src/components/state/User.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+type AuthUser = {
+    name: string
+    email: string
+}
+function User() {
+    const btnStyle = { backgroundColor: 'black', color: 'white', padding: 10, margin: 20, fontSize: 20 }
+
+    // usestate: we are saying User Type can like AuthUser
+    const [User, setUser] = useState<AuthUser>({} as AuthUser)
+
+    const handleLogin = () => {
+        setUser({
+            name: "ErMapsh",
+            email: "ermapsh@gmail.com"
+        })
+    }
+    const handleLogout = () => { setUser({} as AuthUser) }
+
+    return (
+        <div>
+            <button style={btnStyle} onClick={handleLogin}>Login</button>
+            <button style={btnStyle} onClick={handleLogout}>Logout</button>
+            {User && <div>User name is {User?.name}</div>}
+            {User && <div>User email is {User?.email}</div>}
+        </div>
+    )
+}
+
+export default User
